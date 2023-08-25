9600d1123b6a3cb79f4752c6fc4d70f72ade988e
sayantandolui97@gmail.com <sayantandolui97@gmail.com>
2023-08-26 04:00:39 +0530

    logout page add home button

--- src/pages/logout/LogOut.jsx
+++ src/pages/logout/LogOut.jsx
@@ -1,8 +1,10 @@
+import {Link} from 'react-router-dom'
 
 function LogOut() {
   return (
     <>
       <h1>This is logout pages</h1>
+      <Link to='/'><button>Home</button></Link>
     </>
   )
 }
